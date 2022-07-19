095ec065d724b9e76cd5da5cc6115c47f44ed581
Andrew Jensen <andrewkjensen@gmail.com>
2022-07-19 15:50:18 -0700

    start html and js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Let's get this party started!");
+"use strict";
+
+
+//functions get search values from form
+function getSearchText() {
+  return $('#search-text').val();
+}
+
+//function call the giphy api
+async function callGiphyApi() {
+  let searchText = getSearchText();
+
+  let giphy = await axios.get('api.giphy.com/v1/gifs/search',
+    {params: {q: searchText}})
+    console.log()
+}
+//function to update the Dom
+
+//function to clear all
+
+//listener on the clear all button
+
+//listener on the submit button to run a controller function
